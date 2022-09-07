5c6a9e075f014b60d444c07e56b403f25d75c44b
Sabtekengineering <ebukadike92@gmail.com>
2022-09-07 14:30:52 -0700

    Trust God

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---  displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in my MySQL server.
-SELECT COUNT(id) FROM first_table WHERE id=89;
+-- script that inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
+INSERT INTO first_table VALUES (89, 'Best School');
