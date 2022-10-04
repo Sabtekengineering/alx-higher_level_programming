dcbd12bdba9f499403c83cf61d230eea98c5d14e
Sabtekengineering <ebukadike92@gmail.com>
2022-10-04 20:00:08 +0000

    Trust God

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+t Rectangle = require('./4-rectangle'); 
+
+module.exports = class Square extends Rectangle { 
+  constructor (size) { 
+    super(size, size); 
+  } 
+
+  double () { 
+    super.double(); 
+  }
+};
